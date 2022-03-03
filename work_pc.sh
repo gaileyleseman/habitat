9bacf6418996a948673c0ef621978bf3b8f468b2
Gailey Leeman <g.leseman@primevision.com>
2022-03-03 10:09:12 +0100

    initial script for setting up my work PC

---
+++ work_pc.sh
@@ -0,0 +1,32 @@
+# change password
+sudo passwd $USER
+
+# installing Atom
+wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+sudo apt-get update
+sudo apt-get install atom
+
+# installing Microsoft Teams Desktop
+sudo apt install -y curl
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/ms-teams stable main" > /etc/apt/sources.list.d/teams.list'
+sudo apt update
+sudo apt install teams
+
+# installing VS Code
+sudo apt install software-properties-common apt-transport-https wget
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code
+
+# installing Spotify
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add -
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install -y spotify-client
+
+# install terminator
+sudo apt install -y terminator
+
+# create an SSH key
+echo -e "\n"|ssh-keygen -t rsa -N ""
