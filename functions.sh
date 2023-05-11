bb9fa7fdd8ba7c57bd7500728b5a2cee255dc2c7
Gailey <gaileyleseman@gmail.com>
2023-05-11 18:09:56 +0200

    added docker buildx

--- functions.sh
+++ functions.sh
@@ -93,7 +93,12 @@ function install_docker(){
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
             https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update -qq
-    sudo apt-get install -qq -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+    sudo apt-get install -qq -y \
+        docker-ce \
+        docker-ce-cli \
+        containerd.io \
+        docker-compose-plugin \
+        docker-buildx-plugin
     echo "installed Docker Engine"
 
     # post-installation steps
