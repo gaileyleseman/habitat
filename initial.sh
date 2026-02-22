9a0e19fb1b180e530c61e9ea32049cfd4494e091
Gailey Leseman <gaileyleseman@gmail.com>
2026-02-22 22:37:23 +0100

    Feature: Replace scripts with ansible setup  (#7)
    * initial ansible setup
    
    * added zen browser
    
    * add docker steps
    
    * add k8s
    
    * wip: sloppy commit, update ansible tasks
    
    * ftr: add chezmoi to sync managed dotfiles
    
    * ftr: replace thefuck with pay-respects
    
    * swap out pre-commit with prek
    
    * replace dotfiles folder with submodule for chezmoi
    
    * fix Nerd Fonts installation
    
    * update Arduino IDE to 2.x.x, App lab, Micropython
    
    * add pixi as a tool
    
    * cleanup for PR

---
+++ initial.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Install Ansible
+sudo apt update
+sudo apt install -y software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install -y ansible
+
+# SSH Server
+sudo apt install -y openssh-server
+sudo systemctl enable ssh
